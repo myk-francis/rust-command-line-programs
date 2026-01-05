73a5dc368cbde7960967df2f6d2e9b666adccbc6
myk <michaelmugendi1707@gmail.com>
2026-01-05 16:01:48 +0300

    Update: need docs better

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,3 +1,37 @@
+use clap::{Arg, Command};
+
 fn main() {
-    println!("{:?}", std::env::args());
+    let matches = Command::new("echor")
+        .version("0.1.0")
+        .author("myk")
+        .about("Echo with more features")
+        .arg(
+            Arg::new("message")
+                .short('m')
+                .help("Input string to echor")
+                .required(true)
+                .num_args(1..)
+                .action(clap::ArgAction::Append),
+        )
+        .arg(
+            Arg::new("omit_newline")
+                .short('n')
+                .long("no_newline")
+                .help("Do not output a newline at the end")
+                .action(clap::ArgAction::SetTrue),
+        )
+        .get_matches();
+
+    if let Some(values) = matches.get_many::<String>("message") {
+        let sentence = values
+            .map(|s| s.as_str())
+            .collect::<Vec<_>>()
+            .join(" ");
+
+        if matches.get_flag("omit_newline") {
+            print!("{}", sentence);
+        } else {
+            println!("{}", sentence);
+        }
+    }
 }
