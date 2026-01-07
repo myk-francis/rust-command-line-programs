67c2d402dfbf394ac007ef8c8e75b3a7619700db
myk <michaelmugendi1707@gmail.com>
2026-01-07 10:38:29 +0300

    Update: added config struct

--- catr/src/lib.rs
+++ catr/src/lib.rs
@@ -1,5 +1,12 @@
 use std::error::Error;
 
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    number_lines: bool,
+    number_nonblank_lines: bool,
+}
+
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
 pub fn run() -> MyResult<()> {
--- catr/src/main.rs
+++ catr/src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = catr::run() {
+        eprintln!("Application error: {}", e);
+        std::process::exit(1);
+    }
 }
