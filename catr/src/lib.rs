bff4cf18f8521d112cff0d6c18445ad8d01f23ea
myk <michaelmugendi1707@gmail.com>
2026-01-07 11:00:07 +0300

    Update: rearranged config struct

--- catr/src/lib.rs
+++ catr/src/lib.rs
@@ -1,15 +1,30 @@
 use std::error::Error;
+use clap::Parser;
 
-#[derive(Debug)]
-pub struct Config {
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct Config {
+    /// Input file(s)
+    #[arg(default_value = "-")]
     files: Vec<String>,
+
+    /// Number lines
+    #[arg(short, long)]
     number_lines: bool,
+
+    /// Number non-empty lines
+    #[arg(short = 'b', long = "number-nonblank")]
     number_nonblank_lines: bool,
 }
 
+
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
-pub fn run() -> MyResult<()> {
-    println!("Hello, world!");
+pub fn run(config: Config) -> MyResult<()> {
+    dbg!(config);
     Ok(())
+}
+
+pub fn get_args() -> MyResult<Config> {
+    Ok(Config::parse())
 }
